0a4caef5829b88fb914e5d82196903aac3d57a69
phanium <91544758+phanen@users.noreply.github.com>
2026-04-24 23:19:41 +0800

    fix: provide tags for git tags (#2504)
    * fix: provide tags for git tags
    
    `opts.fuzzy.prebuilt_binaries` is removed on latest commit
    
    This commit trying to make this work:
    ```
    require('blink.cmp').download({ force = true, tags = '*' }):wait(60000)
    ```
    
    * fix: reduce useless debug.getinfo
    
    * fix: unused callback arg
    
    * fix: avoid crash when libblink_cmp_fuzzy is in used
    
    learn from
    https://github.com/nvim-treesitter/nvim-treesitter/blob/6878ae017d691a0df1c49d8a091c88a33e4c2d50/lua/nvim-treesitter/install.lua#L327
    
    * refactor: drop rename for now
    
    * refactor: rename `filename` to `current_file_path`
    
    ---------
    
    Co-authored-by: Liam Dyer <liamcdyer@gmail.com>

--- lua/blink/cmp/init.lua
+++ lua/blink/cmp/init.lua
@@ -129,7 +129,7 @@ function cmp.build(opts)
 end
 
 --- Downloads the precompiled library if it's not already available
---- @param opts? { force?: boolean }
+--- @param opts? { force?: boolean, match?: string }
 --- @return blink.lib.Task
 function cmp.download(opts)
   return lib.task
@@ -139,7 +139,8 @@ function cmp.download(opts)
 
       logger:notify(vim.log.levels.INFO, 'Downloading blink.cmp precompiled library')
 
-      local git_tag = lib.native.git_tag(debug.getinfo(1, 'S').source:sub(2))
+      local current_file_path = debug.getinfo(1, 'S').source:sub(2)
+      local git_tag = lib.native.git_tag(current_file_path, opts.match)
       if git_tag == nil then error('Missing git tag, have you pinned the version?') end
 
       local platform = lib.native.platform()
@@ -150,10 +151,10 @@ function cmp.download(opts)
         .. '/'
         .. platform.triple
         .. platform.lib_extension
-      local git_commit = lib.native.git_commit(debug.getinfo(1, 'S').source:sub(2))
+      local git_commit = lib.native.git_commit(current_file_path)
       local library_path = lib.native.library_path('blink_cmp_fuzzy', git_commit)
-      return lib.native.download_async(url, library_path, callback):map(function()
-        if not lib.native.load('blink_cmp_fuzzy', lib.native.git_commit(debug.getinfo(1, 'S').source:sub(2))) then
+      return lib.native.download_async(url, library_path):map(function()
+        if not lib.native.load('blink_cmp_fuzzy', lib.native.git_commit(current_file_path)) then
           error('Failed to load downloaded blink.cmp precompiled library')
         end
         logger:notify(vim.log.levels.INFO, 'Successfully loaded downloaded blink.cmp precompiled library')
