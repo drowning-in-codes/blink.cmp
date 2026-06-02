b943982a29f100a4d17b3d2d09dc06b5a0d0f398
Soifou <fleur.fr@gmail.com>
2026-06-02 23:14:06 +0200

    refactor(cmdline): improve type checking

--- lua/blink/cmp/sources/cmdline/init.lua
+++ lua/blink/cmp/sources/cmdline/init.lua
@@ -10,7 +10,7 @@ local constants = require('blink.cmp.sources.cmdline.constants')
 local utils = require('blink.cmp.sources.cmdline.utils')
 local path_lib = require('blink.cmp.sources.path.lib')
 
---- @class blink.cmp.Source
+---@class blink.cmp.CmdlineSource : blink.cmp.Source
 local cmdline = {
   ---@type table<string, vim.api.keyset.get_option_info?>
   options = nvim.get_all_options_info(),
@@ -28,7 +28,7 @@ end
 ---@return boolean
 function cmdline:enabled() return vim.bo.ft == 'vim' or utils.is_command_line({ ':', '@' }) end
 
----@return table
+---@return string[]
 function cmdline:get_trigger_characters() return { ' ', '.', '#', '&', '-', '=', '/', ':', '!', '%', '~' } end
 
 ---@param context blink.cmp.Context
@@ -75,6 +75,7 @@ function cmdline:get_completions(context, callback)
 
   local unique_suffixes = {}
   local unique_suffixes_limit = 2000
+  ---@type string?, string?
   local special_char, vim_expr
 
   local t = task
@@ -95,8 +96,8 @@ function cmdline:get_completions(context, callback)
       -- Input mode (vim.fn.input())
       if utils.is_command_line({ '@' }) then
         local completion_args = vim.split(completion_type, ',', { plain = true })
-        local custom_type = completion_args[1]
-        local completion_func = completion_args[2]
+        local custom_type = completion_args[1] or ''
+        local completion_func = completion_args[2] or ''
 
         -- Handle custom completions
         if vim.startswith(custom_type, 'custom') then
@@ -152,11 +153,13 @@ function cmdline:get_completions(context, callback)
         if special_char == '#' then
           local alt_buf = vim.fn.bufnr('#')
           if alt_buf ~= -1 then
-            local buffers = { [''] = vim.fn.expand('#') } -- Keep the '#' prefix as a completion option
+            local buffers = {
+              [''] = vim.fn.expand('#') --[[@as string]],
+            } -- Keep the '#' prefix as a completion option
             local curr_buf = nvim.get_current_buf()
             for _, buf in ipairs(vim.fn.getbufinfo({ bufloaded = 1, buflisted = 1 })) do
               if buf.bufnr ~= curr_buf and buf.bufnr ~= alt_buf then
-                buffers[tostring(buf.bufnr)] = vim.fn.expand('#' .. buf.bufnr)
+                buffers[tostring(buf.bufnr)] = vim.fn.expand('#' .. buf.bufnr) --[[@as string]]
               end
             end
             completions = vim.tbl_keys(buffers)
@@ -213,7 +216,8 @@ function cmdline:get_completions(context, callback)
 
         -- current (%) or alternate (#) filename with optional modifiers (:)
         if is_filename_modifier_completion then
-          local expanded = vim.fn.expand(vim_expr .. completion)
+          ---@cast vim_expr string
+          local expanded = vim.fn.expand(vim_expr .. completion) --[[@as string]]
           -- expand in command (e.g. :edit %) but don't in expression (e.g. =vim.fn.expand("%"))
           new_text = vim_expr:sub(1, 1) == current_arg_prefix:sub(1, 1) and expanded or current_arg_prefix .. completion
 
@@ -285,6 +289,7 @@ function cmdline:get_completions(context, callback)
         end
 
         ---@type blink.cmp.CompletionItem
+        ---@diagnostic disable-next-line: missing-fields
         local item = {
           label = label or filter_text,
           filterText = filter_text,
@@ -306,12 +311,12 @@ function cmdline:get_completions(context, callback)
         }
         items[#items + 1] = item
 
-        if option_info and option_info.type == 'boolean' then
+        if completion_type == 'option' and option_info and option_info.type == 'boolean' then
           filter_text = 'no' .. filter_text
           items[#items + 1] = vim.tbl_deep_extend('force', {}, item, {
             label = filter_text,
             filterText = filter_text,
-            labelDetails = { description = 'no' .. label_details.description },
+            labelDetails = { description = 'no' .. option_info.shortname },
             sortText = filter_text,
             textEdit = { newText = 'no' .. new_text },
           }) --[[@as blink.cmp.CompletionItem]]
@@ -322,7 +327,6 @@ function cmdline:get_completions(context, callback)
         is_incomplete_backward = completion_type ~= 'help',
         is_incomplete_forward = is_man_completion and current_arg == '' or false,
         items = items,
-        ---@diagnostic disable-next-line: missing-return
       })
     end)
     :catch(function(err)
@@ -330,7 +334,7 @@ function cmdline:get_completions(context, callback)
       if type(err) ~= 'string' or not utils.is_expected_vim_error(err, { '220', '433' }) then
         logger:notify(vim.log.levels.ERROR, 'Error while fetching completions: ' .. err)
       end
-      ---@diagnostic disable-next-line: missing-return
+
       callback({ is_incomplete_backward = false, is_incomplete_forward = false, items = {} })
     end)
 
